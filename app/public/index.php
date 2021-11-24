b5e033431cc5cc88457829d805add404108cc2c4
tommykarjamenawi <tommy_12345@live.nl>
2021-11-24 10:22:18 +0100

    test

--- app/public/index.php
+++ app/public/index.php
@@ -1,3 +1,36 @@
 <?php
 echo "Requested URL: " . $_SERVER['REQUEST_URI'];
 phpinfo();
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Week 3 Practise</title>
+    <!--<link rel="stylesheet" href="../week3/style.css">-->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+</head>
+<body>
+    <h1>Login Page</h1>
+    #bootstrap login page
+    <div class="container">
+        <div class="row">
+            <div class="col-md-6">
+                <form action="login.php" method="post">
+                    <div class="form-group">
+                        <label for="username">Username</label>
+                        <input type="text" class="form-control" id="username" name="username" placeholder="Enter username">
+                    </div>
+                    <div class="form-group">
+                        <label for="password">Password</label>
+                        <input type="password" class="form-control" id="password" name="password" placeholder="Enter password">
+                    </div>
+                    <button type="submit" class="btn btn-primary">Submit</button>
+                </form>
+            </div>
+        </div>
+</body>
+</html>
